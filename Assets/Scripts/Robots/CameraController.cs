0b90887c8ed92b4e1fb7dc59de4b2be73d2cda60
heller77 <koki001230@icloud.com>
2023-07-15 17:23:15 +0900

    Initial Commit

---
+++ Assets/Scripts/Robots/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Robots
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Vector2 rotate_value;
+
+        public void CameraRotate(InputAction.CallbackContext context)
+        {
+            var rotate_value = context.ReadValue<Vector2>();
+            this.rotate_value = rotate_value;
+            Debug.Log(rotate_value);
+        }
+
+        private void Update()
+        {
+            var noweulerangle = transform.eulerAngles;
+            transform.eulerAngles = new Vector3(noweulerangle.x - rotate_value.y, noweulerangle.y + rotate_value.x,
+                noweulerangle.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Robots/Mover.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Robots
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class Mover : MonoBehaviour
+    {
+        private CharacterController characterController;
+        private Vector3 motion;
+
+        [SerializeField] private float walkspeed;
+
+        Vector3 gravity = Vector3.down;
+
+        private void Start()
+        {
+            this.characterController = this.GetComponent<CharacterController>();
+        }
+
+
+        public void Move(InputAction.CallbackContext context)
+        {
+            var value = context.ReadValue<Vector2>();
+            motion = new Vector3(value.x, 0, value.y) * walkspeed;
+            Debug.Log(motion);
+        }
+
+        private void Update()
+        {
+            var camera_forward = Camera.main.transform.forward;
+            var camera_right = Camera.main.transform.right;
+            var move = camera_forward * motion.z + camera_right * motion.x;
+            Debug.Log(move + " : " + motion);
+            characterController.Move(move * Time.deltaTime + gravity * (Time.deltaTime));
+        }
+    }
+}
